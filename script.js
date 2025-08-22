8b8339f29c5bf6fbdfadf6f5fea6ed6739fd1382
inglyd <inglydalves@outlook.com>
2025-08-22 14:50:52 -0300

    Adicionando entrada e saida do novo salario

---
+++ script.js
@@ -0,0 +1,9 @@
+const leia = require('readline-sync');
+let valorSalario = leia.questionFloat('Digite o valor do salário R$: ');
+let valorAbono = leia.questionFloat('Digite o valor do abono R$: ');
+let novoSalario = valorSalario + valorAbono;
+let formatadorBrl = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
+
+console.log(`O novo salário é: ${formatadorBrl.format(novoSalario)}`);
+
+
