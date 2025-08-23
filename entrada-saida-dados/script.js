96a7c567035eacf1321559eee8b1070fed786681
inglyd <inglydalves@outlook.com>
2025-08-23 07:27:22 -0300

    Organizando os exercicios em subpastas

--- entrada-saida-dados/script.js
+++ entrada-saida-dados/script.js
@@ -5,5 +5,3 @@ let novoSalario = valorSalario + valorAbono;
 let formatadorBrl = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
 
 console.log(`O novo salário é: ${formatadorBrl.format(novoSalario)}`);
-
-
