4667ea95ae71afac98f17738d14fc2d64a300956
inglyd <inglydalves@outlook.com>
2025-08-25 14:36:35 -0300

    Organizando o código

--- control-flow/maior-menor-igual.js
+++ control-flow/maior-menor-igual.js
@@ -4,12 +4,15 @@ const valorA = leia.questionInt('Digite o número A: ');
 const valorB = leia.questionInt('Digite o número B: ');
 const valorC = leia.questionInt('Digite o número C: ');
 
+const soma = valorA + valorB;
 
-if (valorA + valorB > valorC) {
+if (soma > valorC) {
     console.log('A soma de A + B é maior do que C')
-} else if (valorA + valorB < valorC) {
+} else if (soma < valorC) {
     console.log('A soma de A + B é menor do que C')
-} else if (valorA + valorB === valorC) {
+} else if (soma === valorC) {
     console.log('A soma de A + B é igual a C')
-}
+} else {
+    console.log('Valor inválido')
+};
 
